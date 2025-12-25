883748490edd742e044e2871259fd16f98568870
Colin Pollard <colin.pollard1466@gmail.com>
2025-12-25 16:37:17 -0700

    This was the cooler solution to part 2 before I realized I read it all wrong.

---
+++ part_2/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs;
+use std::io;
+use itertools::{Itertools, Position};
+
+fn main() -> io::Result<()>{
+    let file_path = "short_input.txt";
+    let contents = fs::read_to_string(file_path)?;
+
+    let mut invalid_ids: i32 = 0;
+    let mut sum_inv_ids: i64 = 0;
+
+    for range_item in contents.split(",") {
+        let mut num_it = range_item.split("-");
+        let first: i32 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
+        let last: i32 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
+        
+        'num_loop: for num in first..=last {
+            // Test number for repeats. May be easier as a string tbh.
+            let num_str: String  = num.to_string();
+            let len = num_str.len();
+            println!("---------- INPUT: {}", num_str);
+
+            for i in 1..len {
+                // take 0..i segment
+                let test_str: &str = &num_str[0..i];
+
+                println!("TEST STRING: {}", test_str);
+
+                // Check if len(string) % len(0..i) == 0 -> repeats possible
+                if len % test_str.len() == 0 {
+                    // Iterate 
+                    for (pos, element) in num_str.chars().chunks(test_str.len()).into_iter().with_position() {
+                        match pos {
+                            Position::Last | Position::Only => {
+                                let result: String = element.collect();
+                                if result == test_str {
+                                    invalid_ids += 1;
+                                    sum_inv_ids += num as i64;
+
+                                    println!("Found");
+                                    continue 'num_loop;
+                                }
+                            }
+                            Position::First | Position::Middle => {
+                                let result: String = element.collect();
+                                if result != test_str {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("Num thangs {}, sum: {}", invalid_ids, sum_inv_ids);
+    Ok(())
+}
