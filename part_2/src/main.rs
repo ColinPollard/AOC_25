207b64e1a53aa568909fc3e8d82b5e97d843a9ce
Colin Pollard <colin.pollard1466@gmail.com>
2025-12-25 17:00:57 -0700

    NEVERMIND BITCHES I DID IT THE COOL WAY FIRST

--- part_2/src/main.rs
+++ part_2/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use itertools::{Itertools, Position};
 
 fn main() -> io::Result<()>{
     let file_path = "input.txt";
@@ -10,24 +11,42 @@ fn main() -> io::Result<()>{
 
     for range_item in contents.split(",") {
         let mut num_it = range_item.split("-");
-        let first: u64 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
-        let last: u64 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
-
-        for num in first..=last {
+        let first: u128 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
+        let last: u128 = num_it.next().unwrap_or("0").parse().unwrap_or(0);
+        
+        'num_loop: for num in first..=last {
             // Test number for repeats. May be easier as a string tbh.
-            let num_str: String = num.to_string();
+            let num_str: String  = num.to_string();
             let len = num_str.len();
-            
-            if len % 2 != 0 {
-                continue;
-            }
-            
-            let (prefix, suffix) = num_str.split_at(len / 2);            
-            if prefix == suffix {
-                invalid_ids += 1;
-                sum_inv_ids += num as u128;
+            // println!("---------- INPUT: {}", num_str);
 
-                // println!("FOUND {}, {}, {}", num, first, last);
+            for i in 1..len {
+                // take 0..i segment
+                let test_str: &str = &num_str[0..i];
+
+                // Check if len(string) % len(0..i) == 0 -> repeats possible
+                if len % test_str.len() == 0 {
+                    // Iterate 
+                    for (pos, element) in num_str.chars().chunks(test_str.len()).into_iter().with_position() {
+                        match pos {
+                            Position::Last | Position::Only => {
+                                let result: String = element.collect();
+                                if result == test_str {
+                                    invalid_ids += 1;
+                                    sum_inv_ids += num as u128;
+
+                                    continue 'num_loop;
+                                }
+                            }
+                            Position::First | Position::Middle => {
+                                let result: String = element.collect();
+                                if result != test_str {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
