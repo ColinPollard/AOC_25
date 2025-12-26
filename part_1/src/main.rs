b48254cd4cef1a8fe51026e3b98d66ea0cb8cddf
Colin Pollard <colin.pollard1466@gmail.com>
2025-12-25 19:48:45 -0700

    OH GOD I'M ACTUALLY REALLY BAD AND STUPID

--- part_1/src/main.rs
+++ part_1/src/main.rs
@@ -6,31 +6,54 @@ fn main() -> io::Result<()>{
     let contents = fs::read_to_string(file_path)?;
 
     let mut lock_pos: i32 = 50;
-    let mut num_zeros: i32 = 0;
+    let mut num_zeros: i128 = 0;
 
     for line in contents.lines() {
         let prefix: &str = &line[0..1];
         let suffix: &str = &line[1..];
+        println!("{}, {}, {}", lock_pos, prefix, suffix);
 
         let num: i32 = suffix.parse().unwrap_or(0);
+
+        let dumb_flag: bool = lock_pos == 0;
+
         if prefix == "R" {  
+
+            // double counting if it starts at 0 and moves...
             lock_pos += num;
 
             if lock_pos > 99 {
-                lock_pos -= (lock_pos / 100) * 100
+                let rotations = lock_pos / 100;
+                num_zeros += rotations as i128;
+                lock_pos -= rotations * 100;
+                println!("baump: {}", num_zeros);
             }
         }
         else {
             lock_pos -= num;
 
             if lock_pos < 0 {
-                lock_pos -= (lock_pos / 100) * 100
+                let rotations = (lock_pos / 100) - 1; // this one is wrong still. If 53 - 953 -> 10 rotations end at 0 not 100
+                num_zeros -= rotations as i128;
+                lock_pos -= rotations * 100;
+
+                // Please no one look at this code, I too am ashamed.
+                if lock_pos == 100 {
+                    lock_pos = 0;
+                }
+
+                if dumb_flag {
+                    num_zeros -= 1;
+                }
+                println!("baump {}", num_zeros);
+            }
+            else if lock_pos == 0 {
+                num_zeros += 1;
+                println!("baump 3: {}", num_zeros);
             }
         }
 
-        if lock_pos == 0 {
-            num_zeros += 1;
-        }
+        // println!("Position: {}, num_zeros: {}", lock_pos, num_zeros);
     }
 
     println!("Num zeros: {}, lock_pos: {}", num_zeros, lock_pos);
